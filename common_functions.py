4873103fd15bfab6996f7e4d44c8fab2b46587a5
Julia King <28890873+julia-king-edu@users.noreply.github.com>
2024-08-12 16:58:14 +0200

    model training done

--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,8 @@
+from copy import deepcopy
+import pandas as pd
+import json
+import csv
+
 # create helper function that i can include in codeblocks so it will request a confirmation of execution (to prevent unintentional deletion of data or usage of quotas)
 
 def confirm_execution(prompt : str = "The current cell may delete previously gathered data or make a large number of requests. Do you want to execute it?"):
@@ -16,18 +21,14 @@ def confirm_execution(prompt : str = "The current cell may delete previously gat
 
 # write functions to write / load the search_results json (separate function is necessary as it needs to store datetime values. these need to be converted to string before saving and from string when loading)
 
-def write_search_json(search_results : list, location : str = "data/results_search.json"):
+def write_search_json(search_results : list, location : str = "data/search_results.json"):
     """Writes the given object to the results file of the search endpoint. Careful not to overwrite anything!
 
     Args:
         search_results (list): the results of the search endpoint so far
-        location (str, optional): File location. Defaults to "data/results_search.json".
+        location (str, optional): File location. Defaults to "data/search_results.json".
     """
-    
-    from copy import deepcopy
-    import pandas as pd
-    import json
-    
+
     # copy so workspace version is not affected
     sr_copy = deepcopy(search_results)
     
@@ -49,18 +50,15 @@ def write_search_json(search_results : list, location : str = "data/results_sear
     with open(location, "w") as f:
         json.dump(saveable, f, indent = 4)
 
-def read_search_json(location : str = "data/results_search.json") -> list:
+def read_search_json(location : str = "data/search_results.json") -> list:
     """Reads the results of the search endpoint so far.
 
     Args:
-        location (str, optional): File Location. Defaults to "data/results_search.json".
+        location (str, optional): File Location. Defaults to "data/search_results.json".
 
     Returns:
         list: the results of the search endpoint so far
     """
-    
-    import pandas as pd
-    import json
     
     def str_to_datetime(s):
         return pd.to_datetime(s)
@@ -87,20 +85,16 @@ def read_search_json(location : str = "data/results_search.json") -> list:
 
 # write functions to write / load the video data json (separate function is necessary as datetime values are stored. these need to be converted to string before saving and from string when loading)
 
-def write_video_json(video_data : list, location : str = "data/video_data.json"):
+def write_video_json(video_json : list, location : str = "data/videos.json"):
     """Writes the given object to the results file of the video data including metadata and comments. Careful not to overwrite anything!
 
     Args:
-        video_data (list): the video data including metadata and comments generated so far
-        location (str, optional): File location. Defaults to "data/video_data.json".
+        video_json (list): the video data including metadata and comments generated so far
+        location (str, optional): File location. Defaults to "data/videos.json".
     """
     
-    from copy import deepcopy
-    import pandas as pd
-    import json
-    
     # copy so workspace version is not affected
-    vd_copy = deepcopy(video_data)
+    vd_copy = deepcopy(video_json)
     
     def datetime_to_str(dt):
         return dt.isoformat()
@@ -118,18 +112,15 @@ def write_video_json(video_data : list, location : str = "data/video_data.json")
     with open(location, "w") as f:
         json.dump(saveable, f, indent=4)
 
-def read_video_json(location : str = "data/video_data.json") -> list:
+def read_video_json(location : str = "data/videos.json") -> list:
     """Reads the video data including metadata and comments generated so far.
 
     Args:
-        location (str, optional): File Location. Defaults to "data/video_data.json".
+        location (str, optional): File Location. Defaults to "data/videos.json".
 
     Returns:
         list: the video data including metadata and comments generated so far
     """
-    
-    import pandas as pd
-    import json
     
     def str_to_datetime(s):
         return pd.to_datetime(s)
@@ -143,6 +134,75 @@ def read_video_json(location : str = "data/video_data.json") -> list:
         loaded = json.load(f)
 
     # Apply conversion to each dictionary in the list
-    video_data = convert_after_read(loaded)
+    video_json = convert_after_read(loaded)
 
-    return video_data
+    return video_json
+
+# functions to read/write the manual sample
+
+def write_manual_sample(manual_sample : pd.DataFrame, filepath : str = "data/manual_sample.csv"):
+    """Write the manual sample to a csv. Careful not to overwrite!
+
+    Args:
+        manual_sample (pd.DataFrame): manual sample
+        filepath (str, optional): file path to save to. Defaults to "data/manual_sample.csv".
+    """
+    
+    manual_sample.to_csv(filepath, index = False, quoting = csv.QUOTE_MINIMAL)
+    
+def read_manual_sample(filepath : str = "data/manual_sample.csv") -> pd.DataFrame:
+    """Reads manual sample file, mostly here to complete the read/write function set.
+
+    Args:
+        filepath (str, optional): file path to manual sample file. Defaults to "data/manual_sample.csv".
+
+    Returns:
+        pd.DataFrame: maunual sample
+    """
+
+    manual_sample = pd.read_csv(filepath)
+    manual_sample["conspirative"] = pd.to_numeric(manual_sample["conspirative"], errors = "coerce")
+    manual_sample["conspirative"] = manual_sample["conspirative"].astype("boolean")
+    return manual_sample
+
+# functions to read/write the video df
+
+def write_video_df(video_df : pd.DataFrame, filepath : str = "data/videos.csv"):
+    """Write the manual sample to a csv. Careful not to overwrite!
+
+    Args:
+        manual_sample (pd.DataFrame): video df generated in data_processing
+        filepath (str, optional): file path to save to. Defaults to "data/videos.csv".
+    """
+    
+    video_df.to_csv(filepath, quoting = csv.QUOTE_MINIMAL)
+    
+def read_video_df(filepath : str = "data/videos.csv") -> pd.DataFrame:
+    """Reads manual sample file, mostly here to complete the read/write function set.
+
+    Args:
+        filepath (str, optional): file path to manual sample file. Defaults to "data/videos.csv".
+
+    Returns:
+        pd.DataFrame: maunual sample
+    """
+    
+    video_df = pd.read_csv(filepath)
+    # convert label cols
+    label_cols = [col for col in video_df if col.startswith("conspirative_")]
+    for col in label_cols:
+        video_df[col] = pd.to_numeric(video_df[col], errors = "coerce")
+        video_df[col] = video_df[col].astype("boolean")
+    
+    # set id as index
+    video_df = video_df.set_index("id")
+    
+    return video_df
+
+def convert_conspirative_to_bool(df : pd.DataFrame) -> pd.DataFrame:
+    # generate docstring by typing """
+    
+    df_copy = deepcopy(df)
+    label_cols = [col for col in df_copy if col.startswith("conspirative_")]
+    df_copy[label_cols] = df_copy[label_cols].astype("boolean")
+    return df_copy
